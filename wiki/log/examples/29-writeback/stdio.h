0b3567a75324ae0cb7aba87edcad9f7a198ebd59
obijuan <obijuan.cube@gmail.com>
2026-04-19 21:05:25 +0200

    add test-25-puts-error

---
+++ wiki/log/examples/29-writeback/stdio.h
@@ -0,0 +1,30 @@
+#------------------------ PUTS ---------------------------
+#------------------------------------------------
+#-- Imprimir cadena cuya direccion esta indicada
+#-- en el registro proporcionado
+#------------------------------------------------
+# .macro PUTSR(%reg)
+#   mv a0, %reg
+#   jal puts
+# .end_macro
+
+#----------------------------------------
+#-- Se pasa como parametro la etiqueta
+#----------------------------------------
+# .macro PUTSL(%label)
+#   la a0, %label
+#   jal puts
+# .end_macro
+
+#-----------------------------------------------
+#-- Se pasa como paramtro una cadena LITERAL  
+#-----------------------------------------------
+.macro PUTSI str:req
+	 .data
+msg\@: .string "\str"
+
+    .text
+    la a0, msg\@
+    jal puts
+.endm
+
